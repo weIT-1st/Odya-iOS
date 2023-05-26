50e3a43f798680196dc6baf070fa9daa80aad155
Jade Yoo <jeonga038@gmail.com>
2023-05-26 19:02:46 +0900

    FEAT : 프로젝트 생성

---
+++ Odya-iOS/App/Odya_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Odya_iOSApp.swift
+//  Odya-iOS
+//
+//  Created by Jade Yoo on 2023/05/26.
+//
+
+import SwiftUI
+
+@main
+struct Odya_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            LoginView()
+        }
+    }
+}
---
+++ Odya-iOS/Core/Login/View/LoginView.swift
@@ -0,0 +1,26 @@
+//
+//  LoginView.swift
+//  Odya-iOS
+//
+//  Created by Jade Yoo on 2023/05/26.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        LoginView()
+    }
+}
