3b243d64cf9d689db8dbd77a211d5c2e61347a55
heeoh <hio4662@naver.com>
2023-07-10 16:40:28 +0900

    add api utils

---
+++ Odya-iOS/Api/Result/Response.swift
@@ -0,0 +1,26 @@
+//
+//  Response.swift
+//  Odya-iOS
+//
+//  Created by Heeoh Son on 2023/07/10.
+//
+
+import Foundation
+
+// 디코딩 가능한 빈 Response
+struct EmptyResponse: Codable {}
+
+/**
+ 테스트 API를 통해 이름을 전달한 뒤, 해시값과 이름을 받아 저장하는 모델
+ - id: Hash Value
+ - name: Original Name
+ */
+struct TestNetworkResponse: Codable {
+    var id: Int
+    var name: String
+    
+    enum CodingKeys: String, CodingKey {
+        case id = "hashValue"
+        case name = "originalName"
+    }
+}
