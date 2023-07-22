76b7b34634d088b014081b0eb7a231ef5f93997f
heeoh <hio4662@naver.com>
2023-07-22 19:50:49 +0900

    style: color styles 변경

--- Odya-iOS/Theme/ColorStyles.swift
+++ Odya-iOS/Theme/ColorStyles.swift
@@ -9,54 +9,53 @@ import SwiftUI
 
 extension Color {
     // MARK: COLOR
-    static let brandColor   = BrandColor()
-    static let colorScale   = ColorScale()
-    static let system       = System()
+    static let brand        = BrandColor()
+    static let system       = SystemColor()
     
-    // MARK: GRAY SCALE
-    static let background   = Background()
-    static let label        = Label()
-    static let line         = Line()
+    // MARK: GRAY
+    static let label        = LabelColor()
+    static let background   = BackgroundColor()
+    static let line         = LineColor()
+    
+    // MARK: COLOR SCALE (BASE)
+    static let colorscale   = ColorScale()
+    static let grayscale    = GrayScale()
+    static let whiteopacity = WhiteOpacity()
+    static let blackopacity = BlackOpacity()
     
     // MARK: ELEVATION
-    static let elevation    = Elevation()
+    static let elevation    = ElevationColor()
 }
 
 //struct OdyaColor {
-//    // MARK: COLOR
-//    static let brand        = ColorBrand()
-//    static let colorScale   = ColorScale()
-//    static let system       = ColorSystem()
-//
-//    // MARK: GRAY SCALE
-//    static let background   = ColorBackground()
-//    static let label        = ColorLabel()
-//    static let line         = ColorLine()
+//    static let brand        = BrandColor()
+//    static let system       = SystemColor()
+//    static let label        = LabelColor()
+//    static let background   = BackgroundColor()
+//    static let line         = LineColor()
+//    static let elevation    = ElevationColor()
+//    static let colorscale   = ColorScale()
+//    static let grayscale    = GrayScale()
+//    static let whiteopacity = WhiteOpacity()
+//    static let blackopacity = BlackOpacity()
 //}
 
 struct BrandColor {
-    let primary     = Color("primary")
-    let secondary   = Color("secondary")
-    let tertiary    = Color("tertiary")
+    let primary     = Color("base-yellow-50")
+    let secondary   = Color("base-yellow-30")
+    let tertiary    = Color("base-blue-50")
 }
 
-struct ColorScale {
-    let yellow20 = Color("yellow20")
-    let yellow30 = Color("yellow30")
-    let yellow40 = Color("yellow40")
-    let yellow50 = Color("yellow50")
-    let yellow60 = Color("yellow60")
-    let yellow70 = Color("yellow70")
-    
-    let blue20 = Color("blue20")
-    let blue30 = Color("blue30")
-    let blue40 = Color("blue40")
-    let blue50 = Color("blue50")
-    let blue60 = Color("blue60")
-    let blue70 = Color("blue70")
+struct LabelColor {
+    let normal      = Color("base-gray-0")
+    let alternative = Color("base-gray-80")
+    let assistive   = Color("base-gray-50")
+    let inactive    = Color("base-gray-30")
+    let r_normal    = Color("base-gray-50")
+    let r_assistive = Color("base-gray-40")
 }
 
-struct System {
+struct SystemColor {
     let warning             = Color("warning")
     let warningAlternative  = Color("warning_alternative")
     let safe                = Color("safe")
@@ -64,30 +63,80 @@ struct System {
     let inactive            = Color("inactive")
 }
 
-struct Background {
-    let normal = Color("bg_normal")
-    let dimmed = Color("bg_dimmed")
+struct BackgroundColor {
+    let normal          = Color("base-gray-100")
+    let dimmed_system   = Color("base-gray-50")
+    let dimmed_dark     = Color("base-gray-70")
+    let dimmed_light    = Color("base-gray-20")
 }
 
-struct Label {
-    let normal      = Color("label_normal")
-    let alternative = Color("label_alternative")
-    let assistive   = Color("label_assistive")
-    let inactive    = Color("label_inactive")
-    let r_normal    = Color("r_normal")
-    let r_assistive = Color("r_assistive")
+struct LineColor {
+    let normal      = Color("base-white-alpha-40")
+    let alternative = Color("base-white-alpha-10")
 }
 
-struct Line {
-    let normal      = Color("line_normal")
-    let alternative = Color("line_alternative")
+struct ElevationColor {
+    let elev1 = Color("base-gray-100")
+    let elev2 = Color("base-gray-90")
+    let elev3 = Color("base-gray-80")
+    let elev4 = Color("base-gray-70")
+    let elev5 = Color("base-gray-60")
+    let elev6 = Color("base-gray-50")
 }
 
-struct Elevation {
-    let elev1 = Color("elev1")
-    let elev2 = Color("elev2")
-    let elev3 = Color("elev3")
-    let elev4 = Color("elev4")
-    let elev5 = Color("elev5")
-    let elev6 = Color("elev6")
+struct ColorScale {
+    let baseYellow20 = Color("base-yellow-20")
+    let baseYellow30 = Color("base-yellow-30")
+    let baseYellow40 = Color("base-yellow-40")
+    let baseYellow50 = Color("base-yellow-50")
+    let baseYellow60 = Color("base-yellow-60")
+    let baseYellow70 = Color("base-yellow-70")
+    
+    let baseBlue20 = Color("base-blue-20")
+    let baseBlue30 = Color("base-blue-30")
+    let baseBlue40 = Color("base-blue-40")
+    let baseBlue50 = Color("base-blue-50")
+    let baseBlue60 = Color("base-blue-60")
+    let baseBlue70 = Color("base-blue-70")
+}
+
+struct GrayScale {
+    let baseGray100 = Color("base-gray-100")
+    let baseGray90  = Color("base-gray-90")
+    let baseGray80  = Color("base-gray-80")
+    let baseGray70  = Color("base-gray-70")
+    let baseGray60  = Color("base-gray-60")
+    let baseGray50  = Color("base-gray-50")
+    let baseGray40  = Color("base-gray-40")
+    let baseGray30  = Color("base-gray-30")
+    let baseGray20  = Color("base-gray-20")
+    let baseGray10  = Color("base-gray-10")
+    let baseGray5   = Color("base-gray-5")
+    let baseGray0   = Color("base-gray-0")
+}
+
+struct WhiteOpacity {
+    let baseWhiteAlpha90 = Color("base-white-alpha-90")
+    let baseWhiteAlpha80 = Color("base-white-alpha-80")
+    let baseWhiteAlpha70 = Color("base-white-alpha-70")
+    let baseWhiteAlpha60 = Color("base-white-alpha-60")
+    let baseWhiteAlpha50 = Color("base-white-alpha-50")
+    let baseWhiteAlpha40 = Color("base-white-alpha-40")
+    let baseWhiteAlpha30 = Color("base-white-alpha-30")
+    let baseWhiteAlpha20 = Color("base-white-alpha-20")
+    let baseWhiteAlpha10 = Color("base-white-alpha-10")
+    let baseWhiteAlpha5  = Color("base-white-alpha-5")
+}
+
+struct BlackOpacity {
+    let baseBlackAlpha90 = Color("base-black-alpha-90")
+    let baseBlackAlpha80 = Color("base-black-alpha-80")
+    let baseBlackAlpha70 = Color("base-black-alpha-70")
+    let baseBlackAlpha60 = Color("base-black-alpha-60")
+    let baseBlackAlpha50 = Color("base-black-alpha-50")
+    let baseBlackAlpha40 = Color("base-black-alpha-40")
+    let baseBlackAlpha30 = Color("base-black-alpha-30")
+    let baseBlackAlpha20 = Color("base-black-alpha-20")
+    let baseWhiteAlpha10 = Color("base-white-alpha-10")
+    let baseWhiteAlpha5  = Color("base-white-alpha-5")
 }
