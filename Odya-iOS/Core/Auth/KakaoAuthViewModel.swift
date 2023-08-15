cd86ccfa833b184bdb2455d8bf722f48b2c41a35
heeoh <hio4662@naver.com>
2023-08-15 19:27:05 +0900

    style: kakao/appleAuthViewModel 코드 정리

--- Odya-iOS/Core/Auth/KakaoAuthViewModel.swift
+++ Odya-iOS/Core/Auth/KakaoAuthViewModel.swift
@@ -14,7 +14,7 @@ import FirebaseAuth
 
 class KakaoAuthViewModel: ObservableObject {
     
-    // MARK: PROPERTIES
+    // MARK: Properties
     
     @Published var AuthApi = AuthViewModel()
     
@@ -27,94 +27,36 @@ class KakaoAuthViewModel: ObservableObject {
     var kakaoAccessToken: String?
     @AppStorage("WeITAuthToken") var idToken: String?
     
-    /// 사용자 정보
+    /// 로그인한 회원 정보 - 회원가입 시 사용됨
     @Published var userInfo = UserInfo()
     
-    // MARK: FUNCTIONS
+    
+    // MARK: Sign In With Kakao Handling
     
     @MainActor
+    /// 카카오 소셜 로그인 -> 서버 로그인 -> 서버 로그인 성공 시 받아온 토큰으로 파이어베이스 로그인/연동
     func kakaoLogin() {
         idToken = nil
         Task {
-            // 카카오 소셜 로그인
+            // kakao social login
             if (UserApi.isKakaoTalkLoginAvailable()) {
                 await isKakaoLoggedIn = handleKakaoLoginWithKakaoTalk()
             } else {
                 await isKakaoLoggedIn = handleKakaoLoginWithAccount()
             }
             
-            // 카카오 소셜 로그인 실패
+            // kakao social login failed
             guard isKakaoLoggedIn == true,
                   let token = kakaoAccessToken else {
-                print("Error in KakaoAuthViewModel kakaoLogin(): kakao social login failed")
+                print("Error: Kakao social login failed")
                 isLoginFailed = true
                 // TODO: 필요하다면 alert
                 // TODO: 다시 로그인뷰로
                 return
             }
             
-            // 서버로그인
+            // server login & firebase login
             doServerLogin(token: token)
-        }
-    }
-    
-    func doServerLogin(token: String) {
-        AuthApi.kakaoLogin(accessToken: token) { result in
-            switch result {
-            case .success(let firebaseToken):
-                self.doFirebaseLogin(firebaseToken)
-            case .unauthorized(let data):
-                if let newUserData = data?.data {
-                    // 카카오에서 얻은 유저정보 저장
-                    self.userInfo.username = newUserData.username
-                    self.userInfo.nickname = newUserData.nickname
-                    if let email = newUserData.email {
-                        self.userInfo.email = email
-                    }
-                    if let phoneNumber = newUserData.phoneNumber {
-                        self.userInfo.phoneNumber = phoneNumber
-                    }
-                    if let gender = newUserData.gender {
-                        self.userInfo.gender = gender == "M" ? .male : .female
-                    }
-                }
-                // 회원가입 뷰로 연결
-                self.isUnauthorized = true
-            case .error(let errorMessage):
-                print("Error in KakaoAuthViewModel kakaoLogin() - kakao server login faild: \(errorMessage)")
-                self.isLoginFailed = true
-                // TODO: 아마... 로그인 뷰로..? 다시 소셜로그인부터..?
-            }
-        }
-    }
-    
-    func doFirebaseLogin(_ firebaseToken: String) {
-        Auth.auth().signIn(withCustomToken: firebaseToken) { (firebaseSignInResult, error) in
-            if let error = error {
-                print("Error in KakaoAuthViewModel kakaoLogin() - Firebase custom token authentication failed: \(error.localizedDescription)")
-                self.isLoginFailed = true
-                return
-                // TODO: 에러처리 어떻게..? 다시 로그인뷰로..??
-            }
-            
-            // idToken 유효성 확인
-            let currentUser = Auth.auth().currentUser
-            currentUser?.getIDTokenForcingRefresh(true) { idToken, error in
-                if let error = error {
-                    print("Error in KakaoAuthViewModel kakaoLogin(): \(error.localizedDescription)")
-                    self.isLoginFailed = true
-                    return
-                }
-                guard let idToken = idToken else {
-                    print("Error in KakaoAuthViewModel kakaoLogin(): Invalid Token")
-                    self.isLoginFailed = true
-                    return
-                }
-                
-                // idToken을 이용해 서버 로그인
-                self.idToken = idToken
-                return
-            }
         }
     }
     
@@ -153,7 +95,78 @@ class KakaoAuthViewModel: ObservableObject {
     }
     
     
-    // MARK: FUNCTIONS-LOGOUT
+    // MARK: Server Login
+    
+    /// 서버 로그인 api 호출
+    /// 서버 로그인 성공 시 받아온 토큰으로 파이어베이스 로그인/연동
+    /// 등록되지 않은 회원일 경우 회원가입 페이지로 이동
+    func doServerLogin(token: String) {
+        AuthApi.kakaoLogin(accessToken: token) { result in
+            switch result {
+            case .success(let firebaseToken):
+                self.doFirebaseLogin(firebaseToken)
+            case .unauthorized(let data):
+                // 카카오에서 얻은 유저정보 저장
+                if let newUserData = data?.data {
+                    self.userInfo.username = newUserData.username
+                    self.userInfo.nickname = newUserData.nickname
+                    if let email = newUserData.email {
+                        self.userInfo.email = email
+                    }
+                    if let phoneNumber = newUserData.phoneNumber {
+                        self.userInfo.phoneNumber = phoneNumber
+                    }
+                    if let gender = newUserData.gender {
+                        self.userInfo.gender = gender == "M" ? .male : .female
+                    }
+                }
+                // 회원가입 페이지로 연결
+                self.isUnauthorized = true
+            case .error(let errorMessage):
+                print("Error: kakao server login faild: \(errorMessage)")
+                self.isLoginFailed = true
+                // TODO: 아마... 로그인 뷰로..? 다시 소셜로그인부터..?
+            }
+        }
+    }
+    
+    
+    // MARK: Firebase Login
+    
+    /// 파이어베이스 로그인
+    /// 성공 시 토큰 저장
+    func doFirebaseLogin(_ firebaseToken: String) {
+        Auth.auth().signIn(withCustomToken: firebaseToken) { (firebaseSignInResult, error) in
+            if let error = error {
+                print("Error: Firebase custom token authentication failed: \(error.localizedDescription)")
+                self.isLoginFailed = true
+                return
+                // TODO: 에러처리 어떻게..? 다시 로그인뷰로..??
+            }
+            
+            // idToken 유효성 확인
+            let currentUser = Auth.auth().currentUser
+            currentUser?.getIDTokenForcingRefresh(true) { idToken, error in
+                if let error = error {
+                    print("Error: \(error.localizedDescription)")
+                    self.isLoginFailed = true
+                    return
+                }
+                guard let idToken = idToken else {
+                    print("Error: Invalid Token")
+                    self.isLoginFailed = true
+                    return
+                }
+                
+                // idToken을 이용해 서버 로그인
+                self.idToken = idToken
+                return
+            }
+        }
+    }
+    
+    
+    // MARK: Logout
     
     /// 카카오 계정 로그아웃
     @MainActor
@@ -161,7 +174,7 @@ class KakaoAuthViewModel: ObservableObject {
         Task {
             if await handlekakaoLogout() {
                 idToken = nil
-                print("kakao logout seccess")
+                // print("kakao logout seccess")
             }
         }
     }
@@ -169,17 +182,14 @@ class KakaoAuthViewModel: ObservableObject {
     /// 카카오 로그아웃 처리
     func handlekakaoLogout() async -> Bool {
         await withCheckedContinuation{ continuation in
-            UserApi.shared.logout {(error) in
+            UserApi.shared.logout { error in
                 if let error = error {
                     print("Error \(error)")
                     continuation.resume(returning: false)
-                }
-                else {
-                    // print("logout() success.")
+                } else {
                     continuation.resume(returning: true)
                 }
             }
         }
     }
-
 }
