4e8b41f599c441471608a527a6e7c37a9ecccfca
heeoh <hio4662@naver.com>
2023-08-24 16:46:43 +0900

    Style: code formatting

--- Odya-iOS/Core/TravelJournal/ViewModel/DailyJournalEditViewModel.swift
+++ Odya-iOS/Core/TravelJournal/ViewModel/DailyJournalEditViewModel.swift
@@ -8,31 +8,31 @@
 import SwiftUI
 
 class DailyJournalEditViewModel: ObservableObject {
-    @Published var travelJournalEditVM: TravelJournalEditViewModel
-//    @Published var dailyJournal: DailyTravelJournal
-    @Published var dailyJournalList: [DailyTravelJournal] = []
-    
-//    init(dailyJournal: DailyTravelJournal) {
-//        self.dailyJournal = dailyJournal
-//    }
-    
-    init(travelJournalEditVM: TravelJournalEditViewModel) {
-        self.travelJournalEditVM = travelJournalEditVM
-    }
-    
-    func canAddMoreDailyJournals() -> Bool {
-        return dailyJournalList.count <= travelJournalEditVM.duration
-    }
-    
-    func addDailyJournal() {
-        dailyJournalList.append(DailyTravelJournal())
-    }
-    
-    func deleteDailyJournal(dailyJournal: DailyTravelJournal) {
-        dailyJournalList = dailyJournalList.filter{ $0 != dailyJournal }
-    }
-    
-    func setDailyJournalDate(index: Int, selectedDate: Date) {
-        dailyJournalList[index].date = selectedDate
-    }
+  @Published var travelJournalEditVM: TravelJournalEditViewModel
+  //    @Published var dailyJournal: DailyTravelJournal
+  @Published var dailyJournalList: [DailyTravelJournal] = []
+
+  //    init(dailyJournal: DailyTravelJournal) {
+  //        self.dailyJournal = dailyJournal
+  //    }
+
+  init(travelJournalEditVM: TravelJournalEditViewModel) {
+    self.travelJournalEditVM = travelJournalEditVM
+  }
+
+  func canAddMoreDailyJournals() -> Bool {
+    return dailyJournalList.count <= travelJournalEditVM.duration
+  }
+
+  func addDailyJournal() {
+    dailyJournalList.append(DailyTravelJournal())
+  }
+
+  func deleteDailyJournal(dailyJournal: DailyTravelJournal) {
+    dailyJournalList = dailyJournalList.filter { $0 != dailyJournal }
+  }
+
+  func setDailyJournalDate(index: Int, selectedDate: Date) {
+    dailyJournalList[index].date = selectedDate
+  }
 }
--- Odya-iOS/Model/DailyTravelJournal.swift
+++ Odya-iOS/Model/DailyTravelJournal.swift
@@ -8,42 +8,41 @@
 import SwiftUI
 
 struct DailyTravelJournal: Identifiable {
-    var id = UUID()
-    var date: Date? = nil
-    var content: String = ""
-    // 사진 리스트
-    // 태그된 장소 정보
-    
-    func getDayIndex(startDate: Date) -> Int {
-        let calendar = Calendar.current
-        guard let currentDate = self.date else {
-            return 0
-        }
-        let components = calendar.dateComponents([.day], from: startDate, to: currentDate.addDays(1)!)
-        return components.day ?? 0
+  var id = UUID()
+  var date: Date? = nil
+  var content: String = ""
+  // 사진 리스트
+  // 태그된 장소 정보
+
+  func getDayIndex(startDate: Date) -> Int {
+    let calendar = Calendar.current
+    guard let currentDate = self.date else {
+      return 0
     }
+    let components = calendar.dateComponents([.day], from: startDate, to: currentDate.addDays(1)!)
+    return components.day ?? 0
+  }
 }
 
 extension DailyTravelJournal: Equatable {
-    static func == (lhs: DailyTravelJournal, rhs: DailyTravelJournal) -> Bool {
-        return lhs.id == rhs.id
-    }
-    
-    static func != (lhs: DailyTravelJournal, rhs: DailyTravelJournal) -> Bool {
-        return lhs.id != rhs.id
-    }
-    
-   
+  static func == (lhs: DailyTravelJournal, rhs: DailyTravelJournal) -> Bool {
+    return lhs.id == rhs.id
+  }
+
+  static func != (lhs: DailyTravelJournal, rhs: DailyTravelJournal) -> Bool {
+    return lhs.id != rhs.id
+  }
+
 }
 
 extension DailyTravelJournal: Comparable {
-    static func < (lhs: DailyTravelJournal, rhs: DailyTravelJournal) -> Bool {
-        guard let dateL = lhs.date else {
-            return false
-        }
-        guard let dateR = rhs.date else {
-            return true
-        }
-        return dateL < dateR
+  static func < (lhs: DailyTravelJournal, rhs: DailyTravelJournal) -> Bool {
+    guard let dateL = lhs.date else {
+      return false
     }
+    guard let dateR = rhs.date else {
+      return true
+    }
+    return dateL < dateR
+  }
 }
