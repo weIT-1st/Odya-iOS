124c5f6cafe3040c5a481370b05a9346a115b41b
jade-yoo-tr <jeonga038@gmail.com>
2023-09-13 18:27:17 +0900

    FEAT: 약관 api 라우터

---
+++ Odya-iOS/Api/Routers/TermsRouter.swift
@@ -0,0 +1,48 @@
+//
+//  TermsRouter.swift
+//  Odya-iOS
+//
+//  Created by Jade Yoo on 2023/09/13.
+//
+
+import Foundation
+import Moya
+
+enum TermsRouter {
+    case getTermsList
+    case getTermsContent(termsId: Int)
+}
+
+extension TermsRouter: TargetType {
+    var baseURL: URL {
+        return URL(string: ApiClient.BASE_URL)!
+    }
+    
+    var path: String {
+        switch self {
+        case .getTermsList:
+            return "/api/v1/auth/terms"
+        case .getTermsContent(let termsId):
+            return "/api/v1/auth/terms/\(termsId)"
+        }
+    }
+    
+    var method: Moya.Method {
+        switch self {
+        case .getTermsList, .getTermsContent:
+            return .get
+        }
+    }
+    
+    var task: Moya.Task {
+        switch self {
+        default: return .requestPlain
+        }
+    }
+    
+    var headers: [String : String]? {
+        return ["Content-type": "application/json" ]
+    }
+    
+    
+}
---
+++ Odya-iOS/Core/Auth/SignUp/TermsView.swift
@@ -0,0 +1,24 @@
+//
+//  TermsView.swift
+//  Odya-iOS
+//
+//  Created by Jade Yoo on 2023/09/13.
+//
+
+import SwiftUI
+
+struct TermsView: View {
+    // MARK: Properties
+    
+    // MARK: Body
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+// MARK: - Preview
+struct TermsView_Previews: PreviewProvider {
+    static var previews: some View {
+        TermsView()
+    }
+}
