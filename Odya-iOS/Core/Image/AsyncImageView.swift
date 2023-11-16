879d38c539f73fccf02d867120dad96b616e7181
Jade Yoo <jeonga038@gmail.com>
2023-11-16 10:39:37 +0900

    Revert "Revert "Merge branch 'develop' into feature/ODYA-187""
    This reverts commit 5944c27b1216fb0288babbe48b13baad3585714a.

--- Odya-iOS/Core/Image/AsyncImageView.swift
+++ Odya-iOS/Core/Image/AsyncImageView.swift
@@ -13,45 +13,13 @@ struct AsyncImageView: View {
     let height: CGFloat
     let cornerRadius: CGFloat
     
-//    init(url: String, width: CGFloat = 0, height: CGFloat = 0, cornerRadius: CGFloat = Radius.medium) {
-//        self.url = url
-//        self.width = width
-//        self.height = height
-//        self.cornerRadius = cornerRadius
-//    }
-    
     var body: some View {
         AsyncImage(url: URL(string: url)) { image in
-//            if width != 0 {
-//                if height != 0 {
-                    image
-                        .resizable()
-                        .aspectRatio(contentMode: .fill)
-                        .frame(width: width, height: height)
-                        .cornerRadius(cornerRadius)
-//                } else {
-//                    image
-//                        .resizable()
-//                        .aspectRatio(contentMode: .fill)
-//                        .frame(width: width)
-//                        .cornerRadius(cornerRadius)
-//                }
-//            } else {
-//                if height != 0 {
-//                    image
-//                        .resizable()
-//                        .aspectRatio(contentMode: .fill)
-//                        .frame(height: height)
-//                        .cornerRadius(cornerRadius)
-//                } else {
-//                    image
-//                        .resizable()
-//                        .aspectRatio(contentMode: .fill)
-//                        .cornerRadius(cornerRadius)
-//                }
-//
-//            }
-            
+            image
+                .resizable()
+                .aspectRatio(contentMode: .fill)
+                .frame(width: width, height: height)
+                .cornerRadius(cornerRadius)
         } placeholder: {
             ProgressView()
                 .frame(width: width, height: height)
