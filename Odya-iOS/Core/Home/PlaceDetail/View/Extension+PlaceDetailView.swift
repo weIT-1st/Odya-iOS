10a127f8a163b3d456f73ff845b0f6db9f07c1c9
Jade Yoo <jeonga038@gmail.com>
2024-01-09 13:39:50 +0900

    REFACTOR: 장소 상세보기 뷰 코드 분리

---
+++ Odya-iOS/Core/Home/PlaceDetail/View/Extension+PlaceDetailView.swift
@@ -0,0 +1,137 @@
+//
+//  Extension+PlaceDetailView.swift
+//  Odya-iOS
+//
+//  Created by Jade Yoo on 2024/01/09.
+//
+
+import SwiftUI
+
+extension PlaceDetailView {
+  // MARK: - TravelJournal
+  var travelJournalPart: some View {
+    VStack(spacing: 28) {
+      PlaceDetailContentTypeSelection(contentType: .journal, destination: $scrollDestination)
+        .id(PlaceDetailContentType.journal)
+      VStack(spacing: 48) {
+        VStack(spacing: 36) {
+          HStack {
+            Text("나의 여행일지")
+              .h4Style()
+              .foregroundColor(.odya.label.normal)
+            Spacer()
+          }
+        }
+        VStack(spacing: 36) {
+          HStack {
+            Text("친구의 여행일지")
+              .h4Style()
+              .foregroundColor(.odya.label.normal)
+            Spacer()
+          }
+        }
+        VStack(spacing: 36) {
+          HStack {
+            Text("추천 여행일지")
+              .h4Style()
+              .foregroundColor(.odya.label.normal)
+            Spacer()
+          }
+        }
+      }
+      .padding(.horizontal, GridLayout.side)
+    }
+  }
+  
+  // MARK: - Review
+  var reviewPart: some View {
+    VStack(spacing: 28) {
+      PlaceDetailContentTypeSelection(contentType: .review, destination: $scrollDestination)
+        .id(PlaceDetailContentType.review)
+      
+      VStack(spacing: 24) {
+        VStack(spacing: 36) {
+          HStack {
+            HStack(spacing: 8) {
+              Text("한줄리뷰")
+                .h5Style()
+                .foregroundColor(.odya.label.normal)
+              Text("3.0")
+                .b2Style()
+                .foregroundColor(.odya.label.assistive)
+            }
+            Spacer()
+            HStack(spacing: 8) {
+              Text("리뷰 총")
+                .b1Style()
+                .foregroundColor(.odya.label.normal)
+              HStack(spacing: 4) {
+                Text("20")
+                  .b2Style()
+                  .foregroundColor(.odya.brand.primary)
+                Text("개")
+                  .b1Style()
+                  .foregroundColor(.odya.label.normal)
+              }
+            }
+          }
+          .padding(.horizontal, GridLayout.side)
+          StarRatingView(rating: .constant(3.0))
+            .disabled(true)
+        } // header
+        
+        VStack(spacing: 16) {
+          Text("\(placeInfo.title)의 방문 경험은 어떠셨나요?")
+            .detail2Style()
+            .foregroundColor(.odya.label.normal)
+          CTAButton(isActive: .active, buttonStyle: .ghost, labelText: "리뷰 작성", labelSize: .L) {
+            showReviewComposeView.toggle()
+          }
+        }
+        .padding(.horizontal, 10)
+        .padding(.vertical, 24)
+        .frame(maxWidth: .infinity)
+        .background(Color.odya.elevation.elev4)
+        
+        // 정렬
+      }
+    }
+  }
+  
+  // MARK: - Community
+  var communityPart: some View {
+    VStack(spacing: 28) {
+      PlaceDetailContentTypeSelection(contentType: .community, destination: $scrollDestination)
+        .id(PlaceDetailContentType.community)
+      
+      LazyVGrid(columns: columns, spacing: 3) {
+        ForEach(viewModel.feedState.content, id: \.communityID) { content in
+          NavigationLink(value: PlaceDetailRoute.feedDetail(id: content.communityID)) {
+            // 1:1 ratio image
+            Color.clear.overlay(
+              AsyncImage(url: URL(string: content.communityMainImageURL)!) { phase in
+                switch phase {
+                case .success(let image):
+                  image
+                    .resizable()
+                    .scaledToFill()
+                default:
+                  ProgressView()
+                }
+              }
+            )
+            .frame(maxWidth: .infinity)
+            .aspectRatio(1, contentMode: .fit)
+            .clipped()
+          }
+          .onAppear {
+            // 다음 페이지 불러오기
+            if content.communityID == viewModel.feedState.lastId {
+              viewModel.fetchAllFeedByPlaceNextPageIfPossible(placeId: placeInfo.placeId)
+            }
+          }
+        }
+      }
+    }
+  }
+}
--- Odya-iOS/Core/Home/PlaceDetail/View/PlaceDetailView.swift
+++ Odya-iOS/Core/Home/PlaceDetail/View/PlaceDetailView.swift
@@ -24,213 +24,78 @@ enum PlaceDetailContentType: Int, CaseIterable {
   }
 }
 
+enum PlaceDetailRoute: Hashable {
+  case feedDetail(id: Int)
+}
+
 /// 장소 상세보기 뷰
 struct PlaceDetailView: View {
   // MARK: Properties
   @EnvironmentObject var placeInfo: PlaceInfo
   @Binding var isPresented: Bool
   
-  @StateObject private var viewModel = PlaceDetailViewModel()
-  @State private var showReviewComposeView: Bool = false
-  @State private var scrollDestination: PlaceDetailContentType = .journal
+  @StateObject var viewModel = PlaceDetailViewModel()
+  @State var showReviewComposeView: Bool = false
+  @State var scrollDestination: PlaceDetailContentType = .journal
   
   /// Grid columns
   var columns = [GridItem(.flexible(), spacing: 3),
                  GridItem(.flexible(), spacing: 3),
                  GridItem(.flexible())]
+  /// 내비게이션 스택 경로
+  @State private var path = NavigationPath()
   
   // MARK: Body
   var body: some View {
     VStack(spacing: 24) {
       navigationBar
-      
-      ScrollViewReader { proxy in
-        ScrollView(.vertical, showsIndicators: false) {
-          LazyVStack(pinnedViews: .sectionHeaders) {
-            Section {
-              
-              ZStack(alignment: .bottomTrailing) {
-                Image("photo_example 1")
-                  .resizable()
-                  .frame(maxWidth: .infinity)
-                  .frame(height: UIScreen.main.bounds.width * 0.586)
-                  .overlay(
-                    LinearGradient(
-                      stops: [
-                        Gradient.Stop(color: .black.opacity(0), location: 0.22),
-                        Gradient.Stop(color: .black.opacity(0.7), location: 1.00),
-                      ],
-                      startPoint: UnitPoint(x: 0.5, y: 0),
-                      endPoint: UnitPoint(x: 0.5, y: 1)
-                    )
-                  )
-                VStack(spacing: 6) {
-                  visitorView
-                  Spacer()
-                  Text("해운대 해수욕장")
-                    .h4Style()
-                    .foregroundColor(.odya.label.normal)
-                    .frame(maxWidth: .infinity)
-                  Text("부산 해운대구 우동")
-                    .detail2Style()
-                    .foregroundColor(.odya.label.assistive)
-                    .frame(maxWidth: .infinity)
+      NavigationStack(path: $path) {
+        ScrollViewReader { proxy in
+          ScrollView(.vertical, showsIndicators: false) {
+            LazyVStack(pinnedViews: .sectionHeaders) {
+              Section {
+                overview
+                
+                VStack(spacing: 48) {
+                  travelJournalPart
+                  Divider()
+                    .padding(.horizontal, GridLayout.side)
+                  reviewPart
+                  Divider()
+                    .padding(.horizontal, GridLayout.side)
+                  communityPart
                 }
-                .padding(.bottom, 20)
-                Button {
-                  // action: 관심장소 설정/해제
-                } label: {
-                  Image("bookmark-off")
-                }
-                .padding(.trailing, 41)
-                .padding(.bottom, 31)
+                .padding(.vertical, 24)
+                
+              } header: {
+                bottomSheetIndicator
               }
-              
-              VStack(spacing: 48) {
-                // MARK: travel journal
-                VStack(spacing: 28) {
-                  PlaceDetailContentTypeSelection(contentType: .journal, destination: $scrollDestination)
-                    .id(PlaceDetailContentType.journal)
-                  VStack(spacing: 48) {
-                    VStack(spacing: 36) {
-                      HStack {
-                        Text("나의 여행일지")
-                          .h4Style()
-                          .foregroundColor(.odya.label.normal)
-                        Spacer()
-                      }
-                    }
-                    VStack(spacing: 36) {
-                      HStack {
-                        Text("친구의 여행일지")
-                          .h4Style()
-                          .foregroundColor(.odya.label.normal)
-                        Spacer()
-                      }
-                    }
-                    VStack(spacing: 36) {
-                      HStack {
-                        Text("추천 여행일지")
-                          .h4Style()
-                          .foregroundColor(.odya.label.normal)
-                        Spacer()
-                      }
-                    }
-                  }
-                  .padding(.horizontal, GridLayout.side)
-                }
-
-                Divider()
-                  .padding(.horizontal, GridLayout.side)
-                
-                // MARK: review
-                VStack(spacing: 28) {
-                  PlaceDetailContentTypeSelection(contentType: .review, destination: $scrollDestination)
-                    .id(PlaceDetailContentType.review)
-                  
-                  VStack(spacing: 24) {
-                    VStack(spacing: 36) {
-                      HStack {
-                        HStack(spacing: 8) {
-                          Text("한줄리뷰")
-                            .h5Style()
-                            .foregroundColor(.odya.label.normal)
-                          Text("3.0")
-                            .b2Style()
-                            .foregroundColor(.odya.label.assistive)
-                        }
-                        Spacer()
-                        HStack(spacing: 8) {
-                          Text("리뷰 총")
-                            .b1Style()
-                            .foregroundColor(.odya.label.normal)
-                          HStack(spacing: 4) {
-                            Text("20")
-                              .b2Style()
-                              .foregroundColor(.odya.brand.primary)
-                            Text("개")
-                              .b1Style()
-                              .foregroundColor(.odya.label.normal)
-                          }
-                        }
-                      }
-                      .padding(.horizontal, GridLayout.side)
-                      StarRatingView(rating: .constant(3.0))
-                        .disabled(true)
-                    } // header
-                    
-                    VStack(spacing: 16) {
-                      Text("\(placeInfo.title)의 방문 경험은 어떠셨나요?")
-                        .detail2Style()
-                        .foregroundColor(.odya.label.normal)
-                      CTAButton(isActive: .active, buttonStyle: .ghost, labelText: "리뷰 작성", labelSize: .L) {
-                        showReviewComposeView.toggle()
-                      }
-                    }
-                    .padding(.horizontal, 10)
-                    .padding(.vertical, 24)
-                    .frame(maxWidth: .infinity)
-                    .background(Color.odya.elevation.elev4)
-                    
-                    // 정렬
-                  }
-                }
-                
-                Divider()
-                
-                // MARK: Community
-                PlaceDetailContentTypeSelection(contentType: .community, destination: $scrollDestination)
-                  .id(PlaceDetailContentType.community)
-                
-                LazyVGrid(columns: columns, spacing: 3) {
-                  ForEach(viewModel.feedState.content, id: \.communityID) { content in
-                    // 1:1 ratio image
-                    Color.clear.overlay(
-                      AsyncImage(url: URL(string: content.communityMainImageURL)!) { phase in
-                        switch phase {
-                        case .success(let image):
-                          image
-                            .resizable()
-                            .scaledToFill()
-                        default:
-                          ProgressView()
-                        }
-                      }
-                    )
-                    .frame(maxWidth: .infinity)
-                    .aspectRatio(1, contentMode: .fit)
-                    .clipped()
-                    .onAppear {
-                      // 다음 페이지 불러오기
-                      if content.communityID == viewModel.feedState.lastId {
-                        viewModel.fetchAllFeedByPlaceNextPageIfPossible(placeId: placeInfo.placeId)
-                      }
-                    }
-                  }
-                }
-              }
-              .padding(.vertical, 24)
-            } header: {
-              bottomSheetIndicator
+            }
+          } // ScrollView
+          .clipShape(RoundedEdgeShape(edgeType: .top))
+          .onChange(of: scrollDestination) { newValue in
+            proxy.scrollTo(scrollDestination)
+          }
+          .background(Color.odya.background.normal)
+          .toolbar(.hidden)
+          .navigationDestination(for: PlaceDetailRoute.self) { route in
+            switch route {
+            case let .feedDetail(communityId):
+              FeedDetailView(path: $path, communityId: communityId)
             }
           }
-        } // ScrollView
-        .clipShape(RoundedEdgeShape(edgeType: .top))
-        .onChange(of: scrollDestination) { newValue in
-          proxy.scrollTo(scrollDestination)
         }
-        .background(Color.odya.background.normal)
+      } // NavigationStack
+      .frame(maxWidth: .infinity)
+      .sheet(isPresented: $showReviewComposeView) {
+        ReviewComposeView(isPresented: $showReviewComposeView, placeId: $placeInfo.placeId)
+          .presentationDetents([.medium])
+          .presentationDragIndicator(.visible)
       }
-    }
-    .frame(maxWidth: .infinity)
-    .sheet(isPresented: $showReviewComposeView) {
-      ReviewComposeView(isPresented: $showReviewComposeView, placeId: $placeInfo.placeId)
-        .presentationDetents([.medium])
-        .presentationDragIndicator(.visible)
-    }
-    .task {
-      viewModel.fetchVisitingUser(placeId: placeInfo.placeId)
-      viewModel.fetchAllFeedByPlaceNextPageIfPossible(placeId: placeInfo.placeId)
+      .task {
+        viewModel.fetchVisitingUser(placeId: placeInfo.placeId)
+        viewModel.fetchAllFeedByPlaceNextPageIfPossible(placeId: placeInfo.placeId)
+      }
     }
   }
   
@@ -284,6 +149,46 @@ struct PlaceDetailView: View {
     }
     .padding(.horizontal, 20)
     .padding(.vertical, 10)
+  }
+  
+  /// 장소 사진, 이름, 주소, 다녀간 친구, 관심장소 설정 버튼 포함
+  private var overview: some View {
+    ZStack(alignment: .bottomTrailing) {
+      Image("photo_example 1")
+        .resizable()
+        .frame(maxWidth: .infinity)
+        .frame(height: UIScreen.main.bounds.width * 0.586)
+        .overlay(
+          LinearGradient(
+            stops: [
+              Gradient.Stop(color: .black.opacity(0), location: 0.22),
+              Gradient.Stop(color: .black.opacity(0.7), location: 1.00),
+            ],
+            startPoint: UnitPoint(x: 0.5, y: 0),
+            endPoint: UnitPoint(x: 0.5, y: 1)
+          )
+        )
+      VStack(spacing: 6) {
+        visitorView
+        Spacer()
+        Text("해운대 해수욕장")
+          .h4Style()
+          .foregroundColor(.odya.label.normal)
+          .frame(maxWidth: .infinity)
+        Text("부산 해운대구 우동")
+          .detail2Style()
+          .foregroundColor(.odya.label.assistive)
+          .frame(maxWidth: .infinity)
+      }
+      .padding(.bottom, 20)
+      Button {
+        // action: 관심장소 설정/해제
+      } label: {
+        Image("bookmark-off")
+      }
+      .padding(.trailing, 41)
+      .padding(.bottom, 31)
+    }
   }
 }
 
