0a50d55969b16140e27315c7a6e3f9afa2519040
heeoh <hio4662@naver.com>
2024-01-23 22:01:11 +0900

    feat: 위치정보 수집 토글 디바이스 설정으로 유도 추가

--- Odya-iOS/Core/Profile/Settings/View/SettingView.swift
+++ Odya-iOS/Core/Profile/Settings/View/SettingView.swift
@@ -6,9 +6,10 @@
 //
 
 import SwiftUI
+import CoreLocation
 
 // MARK: Setting Route
-/// 프로필 뷰에서 NavigationStack으로 이동할 뷰 타입
+/// 환경설정 뷰에서 NavigationStack으로 이동할 뷰 타입
 enum SettingRoute: Hashable {
   case userInfoEditView
   case privacyPolicyView
@@ -17,6 +18,7 @@ enum SettingRoute: Hashable {
 }
 
 // MARK: Notification Setting
+/// 알람 세부 설정
 struct NotificationSetting: Identifiable {
     let id = UUID()
     let title: String
@@ -49,74 +51,100 @@ struct SettingRowCell<Content: View>: View {
 
 // MARK: Setting View
 struct SettingView: View {
-//  @StateObject var termsVM = TermsViewModel()
+  //  @StateObject var termsVM = TermsViewModel()
+  let locationManager = CLLocationManager()
   
   @State private var isAllNotiOn: Bool = false
   @State private var notificationSettings: [NotificationSetting] = [
-          NotificationSetting(title: "커뮤니티 좋아요 알람"),
-          NotificationSetting(title: "커뮤니티 댓글 알람"),
-          NotificationSetting(title: "마케팅 알람")
-      ]
-  @State private var isLocationTrackingOn: Bool = false
-
+    NotificationSetting(title: "커뮤니티 좋아요 알람"),
+    NotificationSetting(title: "커뮤니티 댓글 알람"),
+    NotificationSetting(title: "마케팅 알람")
+  ]
+  var isLocationTrackingOn: Bool {
+    switch locationManager.authorizationStatus {
+    case .authorizedAlways, .authorizedWhenInUse:
+      return true
+    case .notDetermined, .restricted, .denied:
+      return false
+    @unknown default:
+      return false
+    }
+  }
+  
   var body: some View {
     VStack {
       CustomNavigationBar(title: "환경설정")
       
       ScrollView {
         VStack(spacing: 12) {
+          // 회원정보 수정
           SettingRowCell(hasDivider: true) {
             linkButtonView(nextPageTitle: "회원정보 수정",
                            destination: .userInfoEditView)
           }
           
+          // 개인정보 보호 정책 보기
           SettingRowCell(hasDivider: true) {
             linkButtonView(nextPageTitle: "개인정보 보호정책",
                            destination: .privacyPolicyView)
           }
           
+          // 이용약관 보기
           SettingRowCell(hasDivider: true) {
             linkButtonView(nextPageTitle: "이용약관",
                            destination: .serviceTermsView)
           }
           
+          // 알람 설정
           SettingRowCell(hasDivider: true) {
             notiSettingView
           }
           
-          
+          // 오픈소스 보기
           SettingRowCell(hasDivider: true) {
             linkButtonView(nextPageTitle: "오픈소스",
                            destination: .openSourceView)
           }
           
+          // 위치정보 수집 설정
           SettingRowCell(hasDivider: true) {
             locationTrackingConsentView
           }
           
+          // 버전 정보
           SettingRowCell(hasDivider: true) {
-            versionTextView
+            HStack {
+              settingRowTitle("버전 정보")
+              Spacer()
+              Text("1.0.0")
+                .b2Style()
+                .foregroundColor(.odya.label.assistive)
+            }
           }
           
+          // 문의하기
           SettingRowCell(hasDivider: false) {
             HStack {
               settingRowTitle("문의하기")
               Spacer()
-            }.frame(height: 36)
+            }
           }
         }.padding(GridLayout.side)
       }.background(Color.odya.elevation.elev2)
     }
     .background(Color.odya.background.normal)
   }  // body
-
+  
   private func settingRowTitle(_ title: String) -> some View {
     Text(title)
       .b1Style()
       .foregroundColor(.odya.label.normal)
   }
-  
-  private func linkButtonView(nextPageTitle: String,
+}
+
+// MARK: Link Button
+extension SettingView {
+  func linkButtonView(nextPageTitle: String,
                               destination: SettingRoute) -> some View {
     NavigationLink(destination: {
       switch destination {
@@ -145,40 +173,11 @@ struct SettingView: View {
       }.frame(height: 36)
     }
   }
-    
-    
-//    Button(action: {
-//      path.append(destination)
-//    }) {
-//      HStack {
-//        settingRowTitle(nextPageTitle)
-//        Spacer()
-//        Image("direction-right")
-//          .colorMultiply(.odya.label.assistive)
-//      }
-//    }.frame(height: 36)
-//      .navigationDestination(for: SettingRoute.self) { nextView in
-//        switch nextView {
-//        case .userInfoEditView:
-//          UserInfoEditView()
-//            .navigationBarHidden(true)
-//        case .privacyPolicyView:
-//          TextView(title: "개인정보 보호정책",
-//                   content: "개인정보 보호정책 내용")
-//          .navigationBarHidden(true)
-//        case .serviceTermsView:
-//          TextView(title: "이용약관",
-//                   content: "이용약관 내용")
-//          .navigationBarHidden(true)
-//        case .openSourceView:
-//          TextView(title: "오픈소스",
-//                   content: "오픈소스 내용")
-//          .navigationBarHidden(true)
-//        }
-//      }
-//  }
+}
 
-  private var notiSettingView: some View {
+// MARK: Noti Setting View
+extension SettingView {
+  var notiSettingView: some View {
     VStack(alignment: .trailing, spacing: 16) {
       HStack {
         settingRowTitle("알림설정")
@@ -210,31 +209,41 @@ struct SettingView: View {
     }
   }
 
-  private var locationTrackingConsentView: some View {
+}
+
+// MARK: Location Tracking Consent View
+extension SettingView {
+  var locationTrackingConsentView: some View {
     VStack(alignment: .leading, spacing: 4) {
       HStack {
         settingRowTitle("위치 정보 수집")
         Spacer()
-        Toggle(isOn: $isLocationTrackingOn) {
-          HStack {}.frame(width: 0, height: 36)
-        }.toggleStyle(CustomToggleStyle())
+        locationTrackingToggle
       }
-
+      
       Text("해당 기능을 끄면 여행일지의 동선이 간략화됩니다.")
         .detail2Style()
         .foregroundColor(.odya.label.assistive)
         .frame(height: 9)
     }
   }
-
-  private var versionTextView: some View {
-    HStack {
-      settingRowTitle("버전 정보")
-      Spacer()
-      Text("1.0.0")
-        .b2Style()
-        .foregroundColor(.odya.label.assistive)
-    }.frame(height: 36)
+  
+  private var locationTrackingToggle: some View {
+    ZStack(alignment: isLocationTrackingOn ? .trailing : .leading) {
+      RoundedRectangle(cornerRadius: 50)
+        .fill(isLocationTrackingOn ? Color.odya.brand.primary : Color.odya.system.inactive)
+      Circle()
+        .foregroundColor(.white)
+        .dropBoxShadow()
+        .padding(2)
+    }
+    .frame(width: 51, height: 31)
+    .onTapGesture {
+      if let settingsURL = URL(string: UIApplication.openSettingsURLString) {
+        UIApplication.shared.open(settingsURL, options: [:], completionHandler: nil)
+      }
+    }
+    .animation(.default, value: isLocationTrackingOn)
   }
 }
 
