e5d7debca83faefe92bd7b73fe0236d15f7a326b
Jade Yoo <jeonga038@naver.com>
2024-01-26 17:43:05 +0900

    FEAT: 좌표 사진 조회 라우터, 모델

---
+++ Odya-iOS/Api/Routers/CoordinateImageRouter.swift
@@ -0,0 +1,60 @@
+//
+//  CoordinateImageRouter.swift
+//  Odya-iOS
+//
+//  Created by Jade Yoo on 2024/01/26.
+//
+
+import Foundation
+import Moya
+
+enum CoordinateImageRouter {
+  // 좌표 사진 조회
+  case getCoordinateImages(leftLongitude: Double,
+                           bottomLatitude: Double,
+                           rightLongitude: Double,
+                           topLatitude: Double,
+                           size: Int)
+}
+
+extension CoordinateImageRouter: TargetType, AccessTokenAuthorizable {
+  var baseURL: URL {
+    return URL(string: ApiClient.BASE_URL)!
+  }
+  
+  var path: String {
+    switch self {
+    case .getCoordinateImages:
+      return "/api/v1/images/coordinate"
+    }
+  }
+  
+  var method: Moya.Method {
+    return .get
+  }
+  
+  var task: Moya.Task {
+    switch self {
+    case let .getCoordinateImages(leftLongitude, bottomLatitude, rightLongitude, topLatitude, size):
+      var params: [String: Any] = [:]
+      params["leftLongitude"] = leftLongitude
+      params["bottomLatitude"] = bottomLatitude
+      params["rightLongitude"] = rightLongitude
+      params["topLatitude"] = topLatitude
+      params["size"] = size
+      return .requestParameters(parameters: params, encoding: URLEncoding.queryString)
+    }
+  }
+  
+  var headers: [String : String]? {
+    return ["Content-type": "application/json"]
+  }
+  
+  var authorizationType: Moya.AuthorizationType? {
+    return .bearer
+  }
+  
+  var validationType: ValidationType {
+    return .successCodes
+  }
+}
---
+++ Odya-iOS/Model/CoordinateImage.swift
@@ -0,0 +1,27 @@
+//
+//  CoordinateImage.swift
+//  Odya-iOS
+//
+//  Created by Jade Yoo on 2024/01/26.
+//
+
+import Foundation
+
+// MARK: - CoordinateImage
+struct CoordinateImage: Decodable {
+  let imageId: Int
+  let userId: Int
+  let imageUrl: String
+  let placeId: String
+  let latitude: Double
+  let longitude: Double
+  let imageUserType: ImageUserType
+  let journalId: Int
+  let communityId: Int
+}
+
+enum ImageUserType: String, Decodable {
+  case user = "USER"
+  case friend = "FRIEND"
+  case other = "OTHER"
+}
