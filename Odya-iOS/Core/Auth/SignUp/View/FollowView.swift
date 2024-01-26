ace7c42129b104f4772696618999ce199b01cf43
heeoh <hio4662@naver.com>
2024-01-27 03:15:59 +0900

    feat: 연락처 가져오기

--- Odya-iOS/Core/Auth/SignUp/View/FollowView.swift
+++ Odya-iOS/Core/Auth/SignUp/View/FollowView.swift
@@ -6,42 +6,56 @@
 //
 
 import SwiftUI
+import Contacts
 
-private struct UserSuggestionView: View {
+struct Contact {
+  var id: String
+  var phoneNumber: String
+}
+
+//protocol ContactUsable {}
+//extension ContactUsable {
+//
+//
+//  }
+//}
+
+
+private struct UserSuggestionByContactsView: View {
   var body: some View {
     VStack(alignment: .leading, spacing: 24) {
-      if followHubVM.isLoadingSuggestion {  // 로딩 중
-        HStack {
-          Spacer()
-          ProgressView()
-          Spacer()
-        }
-      } else if followHubVM.suggestedUsers.isEmpty {  // 추천 친구 없음
-        HStack {
-          Spacer()
-          Text("추천 가능한 친구가 없어요!!")
-            .detail1Style()
-            .foregroundColor(.odya.label.assistive)
-          Spacer()
-        }.padding(.bottom, 24)
-      } else {  // 추천 친구 있음
-        ScrollView(.horizontal, showsIndicators: false) {
-          HStack(spacing: 32) {
-            ForEach(followHubVM.suggestedUsers) { suggestedUser in
-              SuggestedUserView(user: suggestedUser)
-            }
-          }
-        }
-      }
+//      if followHubVM.isLoadingSuggestion {  // 로딩 중
+//        HStack {
+//          Spacer()
+//          ProgressView()
+//          Spacer()
+//        }
+//      } else if followHubVM.suggestedUsers.isEmpty {  // 추천 친구 없음
+//        HStack {
+//          Spacer()
+//          Text("추천 가능한 친구가 없어요!!")
+//            .detail1Style()
+//            .foregroundColor(.odya.label.assistive)
+//          Spacer()
+//        }.padding(.bottom, 24)
+//      } else {  // 추천 친구 있음
+//        ScrollView(.horizontal, showsIndicators: false) {
+//          HStack(spacing: 32) {
+//            ForEach(followHubVM.suggestedUsers) { suggestedUser in
+//              SuggestedUserView(user: suggestedUser)
+//            }
+//          }
+//        }
+//      }
 
     }
     .padding(.horizontal, GridLayout.side)
     .padding(.vertical, 28)
     .background(Color.odya.elevation.elev2)
     .cornerRadius(Radius.medium)
-    .onAppear {
-      followHubVM.getSuggestion { _ in }
-    }
+//    .onAppear {
+//      followHubVM.getSuggestion { _ in }
+//    }
   }
 
 }
@@ -91,15 +105,20 @@ struct RegisterFollowsView: View {
       }
       .padding(.bottom, 45)
     }
+    .onAppear {
+      getContacts() { contacts in
+        debugPrint(contacts)
+      }
+    }
   }
   
   // MARK: Title
   private var titleText: some View {
     VStack(alignment: .leading, spacing: 10) {
       VStack(alignment: .leading, spacing: 0) {
-          Text("활동중인 친구를")
+        Text("활동중인 친구를")
           .foregroundColor(.odya.label.normal)
-          Text("오댜에서 만나요!")
+        Text("오댜에서 만나요!")
           .foregroundColor(.odya.brand.primary)
       }
       .frame(height: 49)
@@ -111,6 +130,37 @@ struct RegisterFollowsView: View {
         .frame(height: 12)
       
       Spacer()
+    }
+  }
+  
+  func getContacts(completion: @escaping ([Contact]) -> Void) {
+    let store = CNContactStore()
+    var contacts: [Contact] = []
+    
+    // 연락처에 요청할 항목
+    let keys = [CNContactPhoneNumbersKey] as [CNKeyDescriptor]
+    // Request 생성
+    let request = CNContactFetchRequest(keysToFetch: keys)
+    // 연락처 읽을 때 정렬해서 읽어오도록 설정
+    request.sortOrder = CNContactSortOrder.userDefault
+    
+    // 권한체크
+    store.requestAccess(for: .contacts) { granted, error in
+      guard granted else { return }
+      do {
+        // 연락처 데이터 획득
+        try store.enumerateContacts(with: request) { (contact, stop) in
+          guard let phoneNumber = contact.phoneNumbers.first?.value.stringValue else { return }
+          let id = contact.identifier
+          
+          let contactToAdd = Contact(id: id, phoneNumber: phoneNumber)
+          contacts.append(contactToAdd)
+        }
+      } catch let error {
+        print(error.localizedDescription)
+      }
+
+      completion(contacts)
     }
   }
 }
